5abdae7d906e6ddf01fcdc72c0c9c46e7845307c
Petr Kouba <petr.kouba@bimproject.cz>
2020-11-19 23:17:19 +0100

    Added docs.

--- src/net5app/Program.cs
+++ src/net5app/Program.cs
@@ -4,8 +4,15 @@ namespace net5app
 {
     class Program
     {
+        /// <summary>
+        /// Main program entrypoint
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //Determine arguments
+            //1st argument is required [filename]
+            //2nd argument is optional [-json]
             Console.WriteLine("Hello World!");
         }
     }
